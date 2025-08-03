655376c9ff60c249a9fe35c78225b55948ef2b26
JUNJUE1 <jackkoay1@gmail.com>
2025-08-03 12:16:58 +0800

    Update thier css to style 2, and my own connect

--- php/Sign Up.php
+++ php/Sign Up.php
@@ -1,22 +1,4 @@
 <?php
-    include 'conn.php';
-    
-    session_start();
-
-    // Check if the user is logged in
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: login.php");
-        exit();
-    }       
-
-    // Fetch the username from the database
-    $user_id = $_SESSION['user_id'];
-    $sql = "SELECT name FROM users WHERE id = $user_id";
-    $result = mysqli_query($conn, $sql);
-
-    if ($row = mysqli_fetch_assoc($result)) {
-        $username = htmlspecialchars($row['username']);
-    }
 
 ?>
 
@@ -27,13 +9,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up | GoFit</title>
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style2.css">
 
 </head>
 <body>
     <div class="signup-box">
         <div class="signup-text">
             Welcome to GoFit
+            <br>
             Create an account 
             <br>
             <br>
